deaed0b04290f664b41110f01db1338533eb5b65
esalama01 <mehdisalama61@gmail.com>
2026-07-07 13:14:03 +0100

    defined the parse function

--- src/get.go
+++ src/get.go
@@ -12,8 +12,13 @@ type URL_PARSER struct{ //as in https://www.youtube.com/watch?v=WmMa1GVPWZ0
 	Path string
 }
 
-func Parse(url string){//suppose it's of the format [http://eu.httpbin.org/get/mhido]
+func Parse(url string) (URL_PARSER){//suppose it's of the format [http://eu.httpbin.org/get/mhido]
+	host, path, _ := strings.Cut(url[7:], "/") //what's between // and /
 	parser := URL_PARSER{
-		
+		Host : host,
+		Scheme : "HTTP/1.1",
+		Port : 80,
+		Path : path,
 	}
+	return parser
 }
