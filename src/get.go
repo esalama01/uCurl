6f50e9ee0c95433c0f12d61daf29f1c6714d316e
esalama01 <mehdisalama61@gmail.com>
2026-07-07 13:05:22 +0100

    defined the url parser struct

--- src/get.go
+++ src/get.go
@@ -5,8 +5,15 @@ import(
 	"strings"
 )
 
-func Print_protocole(url string){//suppose it's of the format [http://eu.httpbin.org/get]
-	full_address := url[7:]	//what's between // and /
-	address, _, _ := strings.Cut(full_address, "/")
-	fmt.Printf("connecting to %s\nSending request GET /get HTTP/1.1\nHost:%s\nAccept: */*\n",address, address)
+type URL_PARSER struct{ //as in https://www.youtube.com/watch?v=WmMa1GVPWZ0
+	Host string //aka domain name || IP address.
+	Scheme string //protocol(defaults to http in my case.)
+	Port int //sits between host and path. comes after a ":". (if not specified has a default of 80.(cause i m working with http.))
+	Path string
+}
+
+func Parse(url string){//suppose it's of the format [http://eu.httpbin.org/get/mhido]
+	parser := URL_PARSER{
+		
+	}
 }
