059b5d56da7363d46e939cb42c81a4b44b71c157
esalama01 <mehdisalama61@gmail.com>
2026-07-07 13:20:01 +0100

    modified the parse function

--- src/get.go
+++ src/get.go
@@ -1,7 +1,7 @@
 package src
 
 import(
-	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -14,11 +14,18 @@ type URL_PARSER struct{ //as in https://www.youtube.com/watch?v=WmMa1GVPWZ0
 
 func Parse(url string) (URL_PARSER){//suppose it's of the format [http://eu.httpbin.org/get/mhido]
 	host, path, _ := strings.Cut(url[7:], "/") //what's between // and /
+	_,port,found :=  strings.Cut(host, ":")
+
 	parser := URL_PARSER{
 		Host : host,
 		Scheme : "HTTP/1.1",
-		Port : 80,
 		Path : path,
+	}
+	if (found){
+		port, _ := strconv.Atoi(port)
+		parser.Port = port
+	}else{
+		parser.Port = 80
 	}
 	return parser
 }
