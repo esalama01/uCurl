1cff6e4e63a3feba2ceb83129f24937052d67a28
esalama01 <mehdisalama61@gmail.com>
2026-07-07 14:51:00 +0100

    established the conenction

--- src/get.go
+++ src/get.go
@@ -1,14 +1,14 @@
 package src
 
 import(
-	"strconv"
 	"strings"
+	"net"
 )
 
 type URL_PARSER struct{ //as in https://www.youtube.com/watch?v=WmMa1GVPWZ0
 	Host string //aka domain name || IP address.
-	Scheme string //protocol(defaults to http in my case.)
-	Port int //sits between host and path. comes after a ":". (if not specified has a default of 80.(cause i m working with http.))
+	Protocol string //protocol(defaults to http in my case.)
+	Port string //sits between host and path. comes after a ":". (if not specified has a default of 80.(cause i m working with http.))
 	Path string
 }
 
@@ -18,14 +18,30 @@ func Parse(url string) (URL_PARSER){//suppose it's of the format [http://eu.http
 
 	parser := URL_PARSER{
 		Host : host,
-		Scheme : "HTTP/1.1",
+		Protocol : "HTTP/1.1",
 		Path : path,
 	}
 	if (found){
-		port, _ := strconv.Atoi(port)
 		parser.Port = port
 	}else{
-		parser.Port = 80
+		parser.Port = "80"
 	}
 	return parser
+}
+
+func (server URL_PARSER) Connect() (string){ //d'apres https://dzone.com/articles/socket-programming-in-go
+	//establish connection
+	connection, err := net.Dial("tcp", server.Host + ":" + server.Port) //i defaulted it to tcp bec i m not running twitch lol
+	if err != nil {
+        panic(err)
+    }
+	//read the data
+	buffer := make([]byte, 1024)
+	l, err := connection.Read(buffer)
+	if err != nil {
+        panic(err)
+    }
+	response := string(buffer[:l])
+	defer connection.Close()
+	return response
 }
