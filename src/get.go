934e627e9c8f639b624227390c7ac6e5ea03628e
esalama01 <mehdisalama61@gmail.com>
2026-07-09 13:45:49 +0100

    created the verbose func

--- src/get.go
+++ src/get.go
@@ -55,3 +55,14 @@ func (server URL_PARSER) Connect() string{ //d'apres https://dzone.com/articles/
 	defer connection.Close()
 	return response
 }
+
+func Get_verbose(server URL_PARSER)string{
+	response := server.Connect()
+	fmt.Printf(">Sending request GET /get HTTP/1.1\n>Host: %s\n>Accept: */*",server.Host)
+	lines := strings.Split(response, "\n")
+	for i, line := range lines {
+		lines[i] = "<" + line
+	}
+	result := strings.Join(lines, "\n")
+	return result
+}
